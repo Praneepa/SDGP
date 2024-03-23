73c88227c2b5ef264367435d32e438f67e3dc369
J.K.T. Sandeepa <138207937+Tharusha-Sandeepa@users.noreply.github.com>
2024-03-23 22:49:54 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,28 +1,38 @@
-const express = require("express");
-const path = require("path");
+// Importing required modules
+const express = require("express"); // Express framework for Node.js
+const path = require("path"); // Module for working with file and directory paths
 
+// Creating an Express application
 const app = express();
 
-const userRoute = require("./routes/usersRoute");
-const adminRoute = require("./routes/adminsRoute");
-const projectRoute = require("./routes/projectsRoute");
-const emailRoute = require("./routes/emailRoute");
+// Importing route modules
+const userRoute = require("./routes/usersRoute"); // Users routes
+const adminRoute = require("./routes/adminsRoute"); // Admins routes
+const projectRoute = require("./routes/projectsRoute"); // Projects routes
+const emailRoute = require("./routes/emailRoute"); // Email routes
 
-const dbconfig = require("./db");
+// Importing database configuration
+const dbconfig = require("./db"); // Database configuration
 
+// Serving static files from the 'uploads' directory
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+// Parsing incoming request bodies in JSON format
 app.use(express.json());
 
-app.use("/api/users", userRoute);
-app.use("/api/admins", adminRoute);
-app.use("/api/projects", projectRoute);
-app.use("/api/email", emailRoute);
+// Mounting route handlers
+app.use("/api/users", userRoute); // Mounting users routes
+app.use("/api/admins", adminRoute); // Mounting admins routes
+app.use("/api/projects", projectRoute); // Mounting projects routes
+app.use("/api/email", emailRoute); // Mounting email routes
 
-const port = process.env.PORT || 5000;
+// Setting up the port for the server to listen on
+const port = process.env.PORT || 5000; // Default port is 5000, can be overridden by environment variable
 
+// Serving static files in production mode
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
 }
 
+// Starting the server and listening on the specified port
 app.listen(port, () => console.log("Node Server Started using Nodemon!"));
