99ff7d6d9d84310f40887290036f9a9cfe62218b
Praneepa <124456381+Praneepa@users.noreply.github.com>
2024-03-24 16:24:54 +0530

    SDGP | Vercel deploy

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -6,9 +6,9 @@ mongoose.set("strictQuery", true);
 var mongoURL = "mongodb+srv://strider:strider123@strider.zfa7jt6.mongodb.net/";
 
 // Connecting to MongoDB database using the provided URI or the fallback URL.
-mongoose.connect(process.env.MONGODB_URI || mongoURL, {
-  useUnifiedTopology: true,                                // Using unified topology to manage connections.
-  useNewUrlParser: true,                                  // Using new URL parser for connection string parsing.
+mongoose.connect( mongoURL, {
+  useUnifiedTopology: true,                                
+  useNewUrlParser: true,                                  
 });
 
 // Storing the connection instance in a variable for event handling.
@@ -16,12 +16,12 @@ var connection = mongoose.connection;
 
 // Event handler for MongoDB connection error.
 connection.on("error", () => {
-  console.log("MongDB Connection Failed");                // Logging connection failure.
+  console.log("MongDB Connection Failed");                
 });
 
 // Event handler for successful MongoDB connection.
 connection.on("connected", () => {
-  console.log("MongoDB Connection Successful");           // Logging connection success.
+  console.log("MongoDB Connection Successful");          
 });
 
 module.exports = mongoose;
