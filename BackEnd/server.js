66edf4705f130f27ddea162e0e93784961f5b1e5
Tharusha-Sandeepa <138207937+Tharusha-Sandeepa@users.noreply.github.com>
2024-03-24 15:24:05 +0530

    SDGP | vercel deploy

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,38 +1,41 @@
-// Importing required modules
-const express = require("express"); // Express framework for Node.js
-const path = require("path"); // Module for working with file and directory paths
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const path = require("path");
 
-// Creating an Express application
 const app = express();
 
-// Importing route modules
-const userRoute = require("./routes/usersRoute"); // Users routes
-const adminRoute = require("./routes/adminsRoute"); // Admins routes
-const projectRoute = require("./routes/projectsRoute"); // Projects routes
-const emailRoute = require("./routes/emailRoute"); // Email routes
+const userRoute = require("./routes/usersRoute");
+const adminRoute = require("./routes/adminsRoute");
+const projectRoute = require("./routes/projectsRoute");
+const emailRoute = require("./routes/emailRoute");
 
-// Importing database configuration
-const dbconfig = require("./db"); // Database configuration
+app.use(helmet());
+app.use(cors());
 
-// Serving static files from the 'uploads' directory
+app.use(
+  cors({
+    origin: ["https://lpc-lipton.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
+
+const dbconfig = require("./db");
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Parsing incoming request bodies in JSON format
 app.use(express.json());
 
-// Mounting route handlers
-app.use("/api/users", userRoute); // Mounting users routes
-app.use("/api/admins", adminRoute); // Mounting admins routes
-app.use("/api/projects", projectRoute); // Mounting projects routes
-app.use("/api/email", emailRoute); // Mounting email routes
+app.use("/api/users", userRoute);
+app.use("/api/admins", adminRoute);
+app.use("/api/projects", projectRoute);
+app.use("/api/email", emailRoute);
 
-// Setting up the port for the server to listen on
-const port = process.env.PORT || 5000; // Default port is 5000, can be overridden by environment variable
+const port = process.env.PORT || 5000;
 
-// Serving static files in production mode
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
 }
 
-// Starting the server and listening on the specified port
 app.listen(port, () => console.log("Node Server Started using Nodemon!"));
